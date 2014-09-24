3f67a9da7bcddd2cc0f96b3db91c8f235f542ade
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-09-24 17:21:36 -0400

    Fullscreen Leaflet map

---
+++ js/main.js
@@ -0,0 +1,8 @@
+(function() {
+  var map = L.map('map').setView([42.360183,-71.090469], 17);
+
+  // add an OpenStreetMap tile layer
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+})();
