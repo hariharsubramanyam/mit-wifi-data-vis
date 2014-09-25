a83a1786c9aedc63256a533bc00393cf1d732b1a
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-09-24 21:07:26 -0400

    CSS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 (function() {
-  var map = L.map('map').setView([42.360183,-71.090469], 17);
+  $(document).ready(function() {
+    var map = L.map('map').setView([42.360183,-71.090469], 17);
 
-  // add an OpenStreetMap tile layer
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+    // add an OpenStreetMap tile layer
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  });
 })();
