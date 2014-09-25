58509cf673760c3ba6109834eaaec471647ccf93
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-09-24 21:48:44 -0400

    Added references to other DOM elements

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
   var am_pm;
   var time_hr;
   var time_min;
+  var go_to_time_button;
+  var animate_button;
 
   var am_pm_handler = function() {
     if (am_pm.text() == "PM") {
@@ -13,6 +15,11 @@
 
   $(document).ready(function() {
     am_pm = $("#am-pm");
+    time_hr = $("#time-hr");
+    time_min = $("#time-min");
+    go_to_time_button = $("#go-to-time");
+    animate_button = $("#animate");
+
     am_pm.click(am_pm_handler);
 
     var map = L.map('map').setView([42.360183,-71.090469], 17);
