cc7fc35f50f24152645ce5c76dd162e99f54be35
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-09-24 21:13:44 -0400

    AM PM click handler

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 (function() {
+  var am_pm;
+  var time_hr;
+  var time_min;
+
+  var am_pm_handler = function() {
+    if (am_pm.text() == "PM") {
+      am_pm.text("AM");
+    } else {
+      am_pm.text("PM");
+    }
+  };
+
   $(document).ready(function() {
+    am_pm = $("#am-pm");
+    am_pm.click(am_pm_handler);
+
     var map = L.map('map').setView([42.360183,-71.090469], 17);
 
     // add an OpenStreetMap tile layer
